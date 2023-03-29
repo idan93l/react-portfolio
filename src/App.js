828ef3682ed6cfc6734cb16715069431c49700f5
Idan Lev <idan.93lev@gmail.com>
2023-03-29 10:25:13 +0300

    added states at app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery"
+
 function App() {
+  const [selectedPage, seSelectedPage] = useState('home')
+  const isAboveMediaQuery = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="App">
+    <div className="app ">
     </div>
   );
 }

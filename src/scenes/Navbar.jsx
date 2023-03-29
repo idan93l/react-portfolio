3fd39ae70f11f8ad67a47e2aab039f8f74f448f8
Idan Lev <idan.93lev@gmail.com>
2023-03-29 11:05:30 +0300

    continue working on navbar jsx

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -2,15 +2,41 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Navbar = ({selectedPage, setSelectedPage}) => {
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+      hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
-  const isAboveMediaQuery = useMediaQuery("(min-width: 768px)");
-  
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
   return (
     <nav className={`z-40 w-full fixed top-0 py-6 `}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-
+        <h4 className="font-playfair text-3xl font-bold">IL</h4>
       </div>
+      {/* DESKTOP NAV */}
+      {isAboveSmallScreens ? (
+        <div className="flex justify-between gap-16 font-opensans text-sm font-semibold"></div>
+      ) : (
+        <div>
+          <Link 
+            page="Home"
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        </div>
+      )}
     </nav>
   );
 };
